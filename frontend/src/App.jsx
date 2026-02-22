c96d0a753fcde6257d8e3b9969c7de90fda10fa0
Rakshit Yadav <yadavrakshit60@gmail.com>
2026-02-22 19:10:12 +0530

    adding basic frontend structure
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

---
+++ frontend/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import axios from "axios"
+
+function App() {
+  const [jobsDesc, setJobDesc] = useState("")
+  const [skills, setSkills] = useState([])
+  const [questions, setQuestion] = useState([])
+
+
+  const analzye = async () => {
+    const formdata = new FormData()       // formdata ek dabba hai jisme data pack kr lia orr usme apne ne jobdesc daal diya and phr fast api ko bheja
+    formdata.append("job_desc", jobsDesc)
+
+    const response = await axios.post("http://localhost:8000/analyze", formdata)
+    setSkills(response.data.skills)
+    setQuestion(response.data.question)
+  }
+  return (
+    <div>
+      <h1>NextPrep</h1>
+
+      <textarea
+        placeholder='Paste Job description here'
+        rows="10"
+        onChange={(e) => setJobDesc(e.target.value)}
+      />
+      <button onClick={analzye}>Analyze</button>
+
+      {skills.length > 0 && (
+        <div>
+          <h2>Extracted Skills</h2>
+          <table>
+            <tr><th>skills</th><th>Category</th></tr>
+            {skills.map((skill) => (
+              <tr key={skill.skill}>
+                <td>{skill.skill}</td>
+                <td>{skill.category}</td>
+              </tr>
+            ))}
+          </table>
+
+        </div>
+      )}
+      {questions.length > 0 && (
+        <div>
+          <h2>Interview Question</h2>
+          {questions.map((q) => (
+            <div key={q.questions}>
+              <p>{q.questions}</p>
+              <span>{q.difficulty}</span>
+              <span>{q.category}</span>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+export default App
